db39aecb61da68942e0fc14a73ad341f63f4f269
hippalus <habiphakan.isler@gmail.com>
2023-06-20 03:00:25 +0200

    refactor KnapsackSolver

--- src/main/java/com/mobiquity/packer/impl/KnapsackSolver.java
+++ src/main/java/com/mobiquity/packer/impl/KnapsackSolver.java
@@ -7,7 +7,7 @@ import com.mobiquity.packer.model.OptimizedPackage;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
-import java.util.Comparator;
+import java.util.Collections;
 import java.util.List;
 
 public final class KnapsackSolver implements PackageSolver {
@@ -23,30 +23,48 @@ public final class KnapsackSolver implements PackageSolver {
     public OptimizedPackage solve(final InboundPackage inboundPackage) {
         final int weightLimit = inboundPackage.weightLimit();
         final List<Item> items = inboundPackage.filteredItems();
-        final int itemSize = items.size();
 
+        final BigDecimal[][] dp = this.calculateDPTable(items, weightLimit);
+
+        final List<Item> selectedItems = this.findSelectedItems(items, dp);
+
+        return new OptimizedPackage(weightLimit, selectedItems);
+    }
+
+    private BigDecimal[][] calculateDPTable(final List<Item> items, final int weightLimit) {
+        final int itemSize = items.size();
         final BigDecimal[][] dp = new BigDecimal[itemSize + 1][weightLimit + 1];
 
         for (int i = 0; i <= itemSize; i++) {
             for (int w = 0; w <= weightLimit; w++) {
                 if (i == 0 || w == 0) {
                     dp[i][w] = BigDecimal.ZERO;
-                } else if (items.get(i - 1).weight().compareTo(BigDecimal.valueOf(w)) <= 0) {
-                    final BigDecimal costWithItem = items.get(i - 1).cost().add(dp[i - 1][w - items.get(i - 1).weight().intValue()]);
-                    final BigDecimal costWithoutItem = dp[i - 1][w];
-                    dp[i][w] = costWithItem.max(costWithoutItem);
                 } else {
-                    dp[i][w] = dp[i - 1][w];
+                    final Item currentItem = items.get(i - 1);
+                    if (currentItem.weight().compareTo(BigDecimal.valueOf(w)) <= 0) {
+                        final BigDecimal costWithItem = currentItem.cost().add(dp[i - 1][w - currentItem.weight().intValue()]);
+                        final BigDecimal costWithoutItem = dp[i - 1][w];
+                        dp[i][w] = costWithItem.max(costWithoutItem);
+                    } else {
+                        dp[i][w] = dp[i - 1][w];
+                    }
                 }
             }
         }
 
+        return dp;
+    }
 
+    private List<Item> findSelectedItems(final List<Item> items, final BigDecimal[][] dp) {
         final List<Item> selectedItems = new ArrayList<>();
-        int i = itemSize;
-        int w = weightLimit;
+        int i = items.size();
+        int w = dp[0].length - 1;
+
         while (i > 0 && w > 0) {
-            if ((dp[i][w].compareTo(dp[i - 1][w]) != 0) || (dp[i][w].compareTo(dp[i - 1][w]) == 0 && (i >= 2 && items.get(i - 1).cost().compareTo(items.get(i - 2).cost()) == 0 && items.get(i - 1).weight().compareTo(items.get(i - 2).weight()) <= 0))) {
+            final BigDecimal currentCell = dp[i][w];
+            final BigDecimal prevCell = dp[i - 1][w];
+
+            if (currentCell.compareTo(prevCell) != 0 || (currentCell.compareTo(prevCell) == 0 && this.isLeastWeightAmongTheSameCostsItems(items, i))) {
                 selectedItems.add(items.get(i - 1));
                 w -= items.get(i - 1).weight().intValue();
             }
@@ -54,9 +72,18 @@ public final class KnapsackSolver implements PackageSolver {
             i--;
         }
 
-        selectedItems.sort(Comparator.comparing(Item::index));
+        Collections.sort(selectedItems);
 
-        return new OptimizedPackage(weightLimit, selectedItems);
+        return selectedItems;
+    }
 
+    private boolean isLeastWeightAmongTheSameCostsItems(final List<Item> items, final int currentIndex) {
+        if (currentIndex >= 2) {
+            final Item currentItem = items.get(currentIndex - 1);
+            final Item prevItem = items.get(currentIndex - 2);
+            return currentItem.cost().compareTo(prevItem.cost()) == 0 && currentItem.weight().compareTo(prevItem.weight()) <= 0;
+        }
+        return false;
     }
 }
+
--- src/main/java/com/mobiquity/packer/model/Item.java
+++ src/main/java/com/mobiquity/packer/model/Item.java
@@ -2,5 +2,10 @@ package com.mobiquity.packer.model;
 
 import java.math.BigDecimal;
 
-public record Item(int index, BigDecimal weight, BigDecimal cost) {
+public record Item(int index, BigDecimal weight, BigDecimal cost) implements Comparable<Item> {
+
+    @Override
+    public int compareTo(final Item o) {
+        return Integer.compare(this.index, o.index());
+    }
 }
